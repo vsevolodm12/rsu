3e29bb6a593bbc00044909c923ba23df449fc5ff
vsevolodm12 <korsenkodanil@gmail.com>
2025-12-28 00:35:40 +0300

    Удалена светлая тема и кнопка переключения темы

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Footer from './Footer'
-import ThemeToggle from './ThemeToggle'
 import './Layout.css'
 
 interface LayoutProps {
@@ -10,9 +9,6 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="layout">
-      <div className="theme-toggle-container">
-        <ThemeToggle />
-      </div>
       <main className="layout-content">{children}</main>
       <Footer />
     </div>
