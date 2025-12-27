b5e44b4912676b5372a2fd4ffd2e4c2e58e2fd56
vsevolodm12 <korsenkodanil@gmail.com>
2025-12-28 00:32:26 +0300

    Добавлена светлая тема и кнопка переключения темы в правом верхнем углу

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Footer from './Footer'
+import ThemeToggle from './ThemeToggle'
 import './Layout.css'
 
 interface LayoutProps {
@@ -9,6 +10,9 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="layout">
+      <div className="theme-toggle-container">
+        <ThemeToggle />
+      </div>
       <main className="layout-content">{children}</main>
       <Footer />
     </div>
